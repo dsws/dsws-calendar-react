f4b73db2e1a5e91633d57c78af1814f350618b48
Dani Shulman <webcore1@gmail.com>
2020-02-29 14:58:10 -0500

    Updates

---
+++ precache-manifest.81d1b69423802bb2e3e973c6de10b733.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80b062db8901b024676640057ce2daf9",
+    "url": "/dsws-calendar-react/index.html"
+  },
+  {
+    "revision": "7c909bb3e09d1554ce73",
+    "url": "/dsws-calendar-react/static/css/main.3e0f1114.chunk.css"
+  },
+  {
+    "revision": "febaaeb54dbb5798881c",
+    "url": "/dsws-calendar-react/static/js/2.b99e443c.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/dsws-calendar-react/static/js/2.b99e443c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c909bb3e09d1554ce73",
+    "url": "/dsws-calendar-react/static/js/main.df667a2e.chunk.js"
+  },
+  {
+    "revision": "75e05297d890c48469d5",
+    "url": "/dsws-calendar-react/static/js/runtime-main.dca5fa54.js"
+  },
+  {
+    "revision": "62fbc8ffa835e34c45020a8dc99d09ca",
+    "url": "/dsws-calendar-react/static/media/logo.62fbc8ff.svg"
+  }
+]);
